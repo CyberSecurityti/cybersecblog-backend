e6a067731fbd53da917d2191b0595c31ae65ea3a
unknown <isilva@PCESSIA106.lumis.com.br>
2022-07-10 22:37:13 -0300

    configurações basicas express

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+//libs
+import express from 'express'
+import bodyParser from 'body-parser'
+import session from 'express-session'
+//configurações
+const PORT=process.env.PORT || 8080
+const app = express()
+
+//header
+    app.use((req, res, next) => {
+         //alterar Acess control para o dominio do blog quando estiver pronto!
+        res.header('Access-Control-Allow-Origin', '*')
+        res.header('Acess-Control-Allow-Headers', 'Origin,X-Requrested-With ,Content-Type, Accept,Autorization');
+
+    if (req.method == 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT,POST,PATH,DELETE,GET')
+        return res.status(200).send({});
+    }
+    next();
+})
+//session
+app.use(session({secret:`asklfadfjskdfjsdfklsdjfklsdjnlcvxviibidff`,cookie:{maxAge:70000}}))
+//json 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json())
+
+
+app.get(`/`,(req,res)=>{
+    res.status(200).send({message:`sucess`})
+})
+
+app.use((req,res,next)=>{
+    res.sendStatus(404)
+   
+})
+
+
+app.listen(PORT,()=>{console.log(`server on in port ${PORT}`)})
+
