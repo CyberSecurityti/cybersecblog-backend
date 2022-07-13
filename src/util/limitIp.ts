e1002d50c1bd91f9e2b3eb9a24242ada31298718
isaias-silva <zackblackmangold@gmail.com>
2022-07-13 12:54:07 -0300

    limitador de request(prevenção a ataques)

--- src/util/limitIp.ts
+++ src/util/limitIp.ts
@@ -1,6 +1,6 @@
 import { rateLimit } from "express-rate-limit";
 export const limiter=rateLimit({
-    max: 2,
+    max: 100,
     windowMs: 60 * 60 * 1000,
     message: "que isso meu fi calma"
 })
